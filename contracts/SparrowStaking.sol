8dff6f95fe8427d44c61e6bcfbab427e9b9b4880
TheGreatHB <kavanaboy@gmail.com>
2022-01-19 16:52:40 +0900

    Add SparrowStaking contract

---
+++ contracts/SparrowStaking.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.5.6;
+
+import "./interfaces/ISparrowStaking.sol";
+import "./klaytn-contracts/math/SafeMath.sol";
+
+contract SparrowStaking is ISparrowStaking {
+    using SafeMath for uint256;
+
+    IInjeolmi public ijm;
+    ISInjeolmi public sIjm;
+
+    IKIP17 public constant nft = IKIP17(0x29d05593116C443da54DaBFB4e5322DEA2fff8Cd);
+    uint256 public constant totalNFTs = 8001;
+    uint256 public constant baseDepositedIjm = 1000 * 1e18;
+
+    bool public initialized;
+    uint256 public initialIjmAmount = totalNFTs * baseDepositedIjm;
+    uint256 public initialSIjmAmountPerId;
+
+    mapping(uint256 => bool) public nftSIjmInitialized;
+    mapping(uint256 => uint256) internal _nftSIjmAmount;
+
+    constructor(IInjeolmi _ijm, ISInjeolmi _sIjm) public {
+        ijm = _ijm;
+        sIjm = _sIjm;
+    }
+
+    function initialize() external {
+        require(!initialized);
+        initialized = true;
+
+        ijm.transferFrom(msg.sender, address(this), initialIjmAmount);
+        ijm.approve(address(sIjm), initialIjmAmount);
+
+        initialSIjmAmountPerId = (sIjm.stake(initialIjmAmount)).div(totalNFTs);
+
+        emit Initialize(msg.sender, initialSIjmAmountPerId);
+    }
+
+    function withdrawReward(uint256 id) external {
+        require(id < totalNFTs);
+        require(nft.ownerOf(id) == msg.sender);
+        if (!nftSIjmInitialized[id]) {
+            _nftSIjmAmount[id] = initialSIjmAmountPerId;
+            nftSIjmInitialized[id] = true;
+        }
+
+        uint256 baseSIjm = sIjm.amountToWithdrawIJM(baseDepositedIjm);
+        uint256 withdrawableSIjm = _nftSIjmAmount[id].sub(baseSIjm);
+        require(withdrawableSIjm > 0);
+
+        _nftSIjmAmount[id] = baseSIjm;
+
+        uint256 withdrawedIjm = sIjm.unstake(withdrawableSIjm);
+        ijm.transfer(msg.sender, withdrawedIjm);
+
+        emit WithdrawReward(msg.sender, id, withdrawedIjm);
+    }
+
+    function withdrawableReward(uint256 id) external view returns (uint256) {
+        return nftSIjmAmount(id).mul(sIjm.ratio()).sub(baseDepositedIjm);
+    }
+
+    function nftSIjmAmount(uint256 id) public view returns (uint256) {
+        return nftSIjmInitialized[id] ? _nftSIjmAmount[id] : initialSIjmAmountPerId;
+    }
+}
---
+++ contracts/interfaces/ISparrowStaking.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.6;
+
+import "./IInjeolmi.sol";
+import "./ISInjeolmi.sol";
+import "../klaytn-contracts/token/KIP17/IKIP17.sol";
+
+interface ISparrowStaking {
+    event Initialize(address indexed initializer, uint256 initialSIjmAmountPerId);
+    event WithdrawReward(address indexed owner, uint256 indexed id, uint256 withdrawedIjm);
+
+    function ijm() external view returns (IInjeolmi);
+    function sIjm() external view returns (ISInjeolmi);
+    function nft() external view returns (IKIP17);
+
+    function totalNFTs() external view returns (uint256);
+    function baseDepositedIjm() external view returns (uint256);
+
+    function initialized() external view returns (bool);
+    function initialIjmAmount() external view returns (uint256);
+    function initialSIjmAmountPerId() external view returns (uint256);
+
+    function nftSIjmInitialized(uint256 id) external view returns (bool);
+
+    function initialize() external;
+    function withdrawReward(uint256 id) external;
+    function withdrawableReward(uint256 id) external view returns (uint256);
+    function nftSIjmAmount(uint256 id) external view returns (uint256);
+}
