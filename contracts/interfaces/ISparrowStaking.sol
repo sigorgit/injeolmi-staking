3aedcf07654c1aea8522f3cb4c7be04978ed3d5f
TheGreatHB <kavanaboy@gmail.com>
2022-01-19 22:01:57 +0900

    Update ISparrowStaking.sol

--- contracts/interfaces/ISparrowStaking.sol
+++ contracts/interfaces/ISparrowStaking.sol
@@ -22,7 +22,7 @@ interface ISparrowStaking {
     function nftSIjmInitialized(uint256 id) external view returns (bool);
 
     function initialize() external;
-    function withdrawReward(uint256 id) external;
-    function withdrawableReward(uint256 id) external view returns (uint256);
+    function withdrawReward(uint256[] calldata ids) external;
+    function withdrawableReward(uint256[] calldata ids) external view returns (uint256);
     function nftSIjmAmount(uint256 id) external view returns (uint256);
 }
